97253d313327ddae1bd0e44421a5dfa4fa7fbfb8
Dmitry Malyshev <dd89268147556@gmail.com>
2024-12-24 21:42:52 +0400

    create structure

---
+++ config/init.php
@@ -0,0 +1,17 @@
+<?php
+
+define("DEBUG", 1);
+define("ROOT", dirname(dirname(__DIR__) . "/.."));
+define("PUBLIC", ROOT . "/public");
+define("APP", ROOT . "/app");
+define("CORE", ROOT . "/vendor/pfm");
+define("HELPERS", ROOT . "/vendor/pfm/helpers");
+define("CACHE", ROOT . "/tmp/cache");
+define("LOGS", ROOT . "/tmp/logs");
+define("CONFIG", ROOT . "/config");
+define("LAYOUT", "ishop");
+define("PATH", "http://dima.ru:8000");
+define("ADMIN", "http://dima.ru:8000");
+define("NO_IMAGE", "uploads/no_image.jpg");
+
+require_once ROOT . "/vendor/autoload.php";
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+if (PHP_MAJOR_VERSION < 8) {
+
+    die("Версия ниже 8");
+} else {
+    echo "Все чики бомбито!";
+}
