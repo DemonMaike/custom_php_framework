2daca9597dd1546646605c4546934bff8e7ea065
Dmitry Malyshev <dd89268147556@gmail.com>
2024-12-27 22:40:21 +0400

    Added Router and routes

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
 
 use Pfm\App;
+use Pfm\Router;
 
 require_once (dirname(__DIR__) . "/config/init.php");
+require_once HELPERS . "/functions.php";
+require_once CONFIG . "/routes.php";
 
 new App();
 
-throw new Exception("Ошибка",500);
+debug(Router::getRoutes());
