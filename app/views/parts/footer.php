8b357dae774cfc187dac767af9d566bf50fc3c43
Dmitry Malyshev <dd89268147556@gmail.com>
2025-01-07 20:54:40 +0400

    Finished structure for main view page

--- app/views/parts/footer.php
+++ app/views/parts/footer.php
@@ -1,3 +1,59 @@
-<?php $this->getDbLogs();?>
-</body>
-</html>
+<?=$this->getDbLogs();?>
+<footer>
+			<section class="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-6">
+							<h4>Информация</h4>
+							<ul class="list-unstyled">
+								<li><a href="#">Главная</a></li>
+								<li><a href="#">О магазине</a></li>
+								<li><a href="#">Оплата и доставка</a></li>
+								<li><a href="#">Контакты</a></li>
+							</ul>
+						</div>
+
+						<div class="col-md-3 col-6">
+							<h4>Время работы</h4>
+							<ul class="list-unstyled">
+								<li>г. Екатеринбург, ул. Пушкина, 10</li>
+								<li>пн-вс: 9:00 - 18:00</li>
+								<li>без перерыва</li>
+							</ul>
+						</div>
+
+						<div class="col-md-3 col-6">
+							<h4>Контакты</h4>
+							<ul class="list-unstyled">
+								<li><a href="tel:5551234567">555 123-45-67</a></li>
+								<li><a href="tel:5551234567">555 123-45-68</a></li>
+								<li><a href="tel:5551234567">555 123-45-69</a></li>
+							</ul>
+						</div>
+
+						<div class="col-md-3 col-6">
+							<h4>Мы в сети</h4>
+							<div class="footer-icons">
+								<a href="#"><i class="fab fa-facebook-f"></i></a>
+								<a href="#"><i class="fab fa-youtube"></i></a>
+								<a href="#"><i class="fab fa-instagram"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</footer>
+
+		<button id="top">
+			<i class="fas fa-angle-double-up"></i>
+		</button>
+
+        <script> const PATH = "<?=PATH?>"</script>
+		<script src="<?= PATH?>/static/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js"
+		integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+		crossorigin="anonymous"></script>
+		<script src="<?= PATH?>/static/js/main.js"></script>
+
+	</body>
+	</html>
